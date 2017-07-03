41a71208c52f13ed5435297e1999d52ef8fc475f
Ryan Carrie <rcarrie42@gmail.com>
2017-07-03 08:31:40 -0600

    Java - Hello-World (not submitted)

--- java/hello-world/src/main/java/HelloWorld.java
+++
@@ -1,5 +0,0 @@
-public class HelloWorld {
-    public static String hello(String name) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
-}
