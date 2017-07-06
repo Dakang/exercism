4b76b6d404af8f57d78dada8acc18dd615cd35d7
Ryan Carrie <rcarrie42@gmail.com>
2017-07-06 13:45:05 -0600

    Need to debug my variables

--- java/rna-transcription/src/main/java/RnaTranscription.java
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,11 +1,29 @@
+import java.util.HashMap;
+
 public class RnaTranscription {
 
     public String transcribe(String dnaStrand) {
-    	Map<String, String> rnaTranscribble = new HashMap<String, String>();
-		rnaTranscribble.put("C", "G");
-		rnaTranscribble.put("G", "C");
-		rnaTranscribble.put("A", "T");
-		rnaTranscribble.put("U", "A");
-    	return rnaTranscribble.get(dnaStrand);
+    	//String variable to hold the transcription
+    	String rnaStrand = new String();
+
+    	//Convert dnaStrand from String to Character Array
+    	char[] dnaArray = dnaStrand.toCharArray();
+
+    	//Initalizing a HashMap to convert from DNA to RNA
+    	HashMap<Character, Character> rna = new HashMap<Character, Character>();
+
+		rna.put("C", "G");
+		rna.put("G", "C");
+		rna.put("A", "T");
+		rna.put("U", "A");
+
+		//Iterate through the DNA Strand and transcribe as needed
+		for(char c : dnaArray) {
+			rnaStrand = rnaStrand + rna.get(c);
+
+		}
+
+		//Returning the proper results
+		return rnaStrand;
     }
 }
--- java/rna-transcription/src/test/java/RnaTranscriptionTest.java
+++ java/rna-transcription/src/test/java/RnaTranscriptionTest.java
@@ -22,25 +22,21 @@ public class RnaTranscriptionTest {
         Assert.assertEquals("G", rnaTranscription.transcribe("C"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testRnaTranscriptionOfGuanineIsCytosine() {
         Assert.assertEquals("C", rnaTranscription.transcribe("G"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testRnaTranscriptionOfThymineIsAdenine() {
         Assert.assertEquals("A", rnaTranscription.transcribe("T"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testRnaTranscriptionOfAdenineIsUracil() {
         Assert.assertEquals("U", rnaTranscription.transcribe("A"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testRnaTranscription() {
         Assert.assertEquals("UGCACCAGAAUU", rnaTranscription.transcribe("ACGTGGTCTTAA"));
