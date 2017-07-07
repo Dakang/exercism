2970581ad8f11f0c3d4ac2cf02f8cecbdebc1700
Ryan Carrie <rcarrie42@gmail.com>
2017-07-07 15:50:47 -0600

    Almost there

--- java/rna-transcription/src/main/java/RnaTranscription.java
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,27 +1,32 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+
 
 public class RnaTranscription {
 
     public String transcribe(String dnaStrand) {
     	//String variable to hold the transcription
-    	String rnaStrand = new String();
+    	ArrayList<Character> rnaArray = new ArrayList<>();
+		String rnaStrand;
 
     	//Convert dnaStrand from String to Character Array
     	char[] dnaArray = dnaStrand.toCharArray();
 
     	//Initalizing a HashMap to convert from DNA to RNA
-    	HashMap<Character, Character> rna = new HashMap<Character, Character>();
+    	HashMap<Character, Character> rna = new HashMap<>();
 
-		rna.put("C", "G");
-		rna.put("G", "C");
-		rna.put("A", "T");
-		rna.put("U", "A");
+		rna.put('C', 'G');
+		rna.put('G', 'C');
+		rna.put('A', 'T');
+		rna.put('U', 'A');
 
 		//Iterate through the DNA Strand and transcribe as needed
 		for(char c : dnaArray) {
-			rnaStrand = rnaStrand + rna.get(c);
-
+			rnaArray.add(rna.get(c));
 		}
+
+		//Convert the Array to a string!
+        rnaStrand = rnaArray.toString();
 
 		//Returning the proper results
 		return rnaStrand;
