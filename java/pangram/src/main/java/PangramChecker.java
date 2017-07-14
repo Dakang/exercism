b2b0f21ebd6d19d93d085b6232b8775891b94a6a
Ryan Carrie <rcarrie42@gmail.com>
2017-07-14 11:34:12 -0600

    Started work on pangram track

---
+++ java/pangram/src/main/java/PangramChecker.java
@@ -0,0 +1,20 @@
+
+
+public class PangramChecker {
+
+    public boolean isPangram(String input) {
+        //Array that contains all letters
+        char[] checkArray = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+        //Convert input to array
+        inputArray = new input.CharArray();
+
+        //Check the input and remove letters from check Array is they are valid
+        for (char c : inputArray) {
+
+        }
+
+
+    }
+
+}
