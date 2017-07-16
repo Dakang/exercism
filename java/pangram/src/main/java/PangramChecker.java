b1494751cbaf74a8970a2b49b7b4081e18b50731
Ryan Carrie <rcarrie42@gmail.com>
2017-07-16 09:06:00 -0600

    pangram nearly completed

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -2,16 +2,26 @@
 
 public class PangramChecker {
 
-    public boolean isPangram(String input) {
-        //Array that contains all letters
-        char[] checkArray = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    char[] checkArray = "abcdefghijklmnopqrstuvwxyz".toCharArray();
 
+    public void compareArrays(Character c) {
+        for (char x : checkArray) {
+            if (c == x) {
+                checkArray.Remove(c);
+                break;
+            }
+        }
+    }
+
+    public boolean isPangram(String input) {
         //Convert input to array
         inputArray = new input.CharArray();
 
         //Check the input and remove letters from check Array is they are valid
         for (char c : inputArray) {
-
+            compareArrays(c);
+                }
+            }
         }
 
 
